be7880b7fa969fbb95bebf908ccf86f9f067ecbd
shubham paul <91714143+Shu12388y@users.noreply.github.com>
2024-04-03 19:51:14 +0530

    file added

--- src/HeroSection/components/EventCard.jsx
+++ src/HeroSection/components/EventCard.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-export function EventCard({img,eventName,date,cityName,weather,distance}) {
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+export function EventCard({
+  img,
+  eventName,
+  date,
+  cityName,
+  weather,
+  distance,
+}) {
   return (
     <div className="relative h-[400px] w-[300px] rounded-md">
-      <img
+      <LazyLoadImage src={img} width={600} height={400}  className="z-0 h-full w-full rounded-md object-cover" alt="Image Alt" />
+      {/* <img
         src={img}
         alt="Event Image"
         className="z-0 h-full w-full rounded-md object-cover"
-      />
+      /> */}
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <div className="absolute bottom-4 left-4 text-left">
-        
-        <h1 className="text-sm  text-white"><span className='text-md'> {eventName}</span>  <span className='pl-[1rem]'>March {date.slice(8,10)}, {date.slice(0,4)}</span></h1>
-        <h1 className="text-sm  text-white"><span>&#x1F4CD;</span>{cityName}  <span className='ml-[3rem]'> </span>{weather} | {distance.slice(0,2)}Km  </h1>
-        
-        
-        
+        <h1 className="text-sm  text-white">
+          <span className="text-md"> {eventName}</span>{" "}
+          <span className="pl-[1rem]">
+            March {date.slice(8, 10)}, {date.slice(0, 4)}
+          </span>
+        </h1>
+        <h1 className="text-sm  text-white">
+          <span>&#x1F4CD;</span>
+          {cityName} <span className="ml-[3rem]"> </span>
+          {weather} | {distance.slice(0, 2)}Km{" "}
+        </h1>
       </div>
     </div>
-  )
+  );
 }
--- src/HeroSection/components/FeatureCard.jsx
+++ src/HeroSection/components/FeatureCard.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
-export function Card({img,distance,cityname,weather,eventName,date}) {
+export function Card({ img, distance, cityname, weather, eventName, date }) {
   return (
     <div className="w-[300px] rounded-md border">
-      <img
+      <LazyLoadImage src={img} width={600} height={400} className="h-[200px] w-full rounded-md object-cover" alt="Image Alt" />
+
+      {/* <img
         src={`${img}`}
         alt="image"
         className="h-[200px] w-full rounded-md object-cover"
-      />
-      <div className='bg-slate-100 p-1 transparent'>March {date.slice(8,10)}, {date.slice(0,4)}</div>
+      /> */}
+      <div className="bg-slate-100 p-1 transparent">
+        March {date.slice(8, 10)}, {date.slice(0, 4)}
+      </div>
       <div className="p-4">
         <h1 className="text-lg font-semibold">{eventName}</h1>
         <p className="mt-3 text-sm text-gray-600">
-          <span> &#x1F4CD;</span>{cityname} <span className='ml-[2rem]'></span>{weather} |  {distance.slice(0,2)}Km
+          <span> &#x1F4CD;</span>
+          {cityname} <span className="ml-[2rem]"></span>
+          {weather} | {distance.slice(0, 2)}Km
         </p>
-       
       </div>
     </div>
-  )
+  );
 }
